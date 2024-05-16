ebb1502278e4d76b7853dc701d11775cdf4ba73c
JavierHN64 <jhernandezn3@dian.gov.co>
2024-05-16 15:45:51 -0500

    comentario categorías javier

--- src/PartsUnlimited.Models/Category.cs
+++ src/PartsUnlimited.Models/Category.cs
@@ -6,6 +6,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace PartsUnlimited.Models
 {
+    //clase para categorías
     public class Category
     {
         public int CategoryId { get; set; }
